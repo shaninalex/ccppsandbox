7357ca3bc4dc95fd1a978f3e3bcd954a5090d523
shaninalex <shanin.x@gmail.com>
2023-04-23 23:40:18 +0300

    add opengl

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 build:
-	g++ -std=c++17 main.cpp -o prog -lSDL2
+	g++ -std=c++17 main.cpp glad/src/glad.c -I./glad/include -o prog -lSDL2 -ldl
 	./prog
 
 
--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
+#include <SDL2/SDL_video.h>
 #include <iostream>
 #include <SDL2/SDL.h>
+
+// Include GLAD
+#include "glad/include/glad/glad.h"
+
+
 
 int main(int argc, char* argv[]) {
     SDL_Window* window = nullptr;
@@ -10,16 +16,31 @@ int main(int argc, char* argv[]) {
         std::cout << "SDL video system is ready to go\n";
     }
 
+    // specify OpenGL version
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+
+
     window = SDL_CreateWindow(
         "C++ SDL2 Window", 
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
         640, 480,
-        SDL_WINDOW_SHOWN);
+        SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
+
+    SDL_GLContext context;
+    context = SDL_GL_CreateContext(window);
+
+    gladLoadGLLoader(SDL_GL_GetProcAddress);
 
     // Main application loop
     bool gameIsRunning = true;
     while(gameIsRunning) {
+        glViewport(0,0, 640, 480);
         SDL_Event event;
 
         // start our event loop
@@ -46,6 +67,11 @@ int main(int argc, char* argv[]) {
             if (state[SDL_SCANCODE_RIGHT]) {
                 std::cout << "right arrow key was pressed\n";
             }
+
+            glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+            glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+
+            SDL_GL_SwapWindow(window);
         }
     }
 
